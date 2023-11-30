83417176278a0539bea84a7ade97032277246ed5
yahay05 <94457180+yahay05@users.noreply.github.com>
2023-12-01 00:45:46 +0400

    add stripe webhook and html element for email

--- server.ts
+++ server.ts
@@ -4,6 +4,9 @@ import { nextApp, nextHandler } from "./next-utils"
 import * as trpcExpress from "@trpc/server/adapters/express"
 import { appRouter } from "./trpc"
 import { inferAsyncReturnType } from "@trpc/server"
+import bodyParser from "body-parser"
+import { IncomingMessage } from "http"
+import { stripeWebhookHandler } from "./webhooks"
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
@@ -11,7 +14,18 @@ const PORT = Number(process.env.PORT) || 3000
 const createContext = ({req,res}: trpcExpress.CreateExpressContextOptions) => ({req,res})
 export type ExpressContext = inferAsyncReturnType<typeof createContext>
 
+export type WebhookRequest = IncomingMessage & {rawBody: Buffer}
+
 const start = async () => {
+
+    const webhookMiddleware = bodyParser.json({
+        verify: (req: WebhookRequest, _, buffer) => {
+            req.rawBody = buffer
+        }
+    })
+
+    app.post("/api/webhooks/stripe", webhookMiddleware, stripeWebhookHandler)
+
     const payload = await getPayloadClient({
         initOptions: {
             express: app,
