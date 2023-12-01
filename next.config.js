77342747cac78afadf89012641e98958a7b983a7
yahay05 <94457180+yahay05@users.noreply.github.com>
2023-12-01 07:06:55 +0400

    changed next.config.js for image rendering

--- next.config.js
+++ next.config.js
@@ -3,16 +3,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: "localhost",
-        pathname: "**",
-        port: "3000",
         protocol: "http",
+        hostname: "localhost",
       },
       {
-        hostname: "digitalhippo-production-783c.up.railway.app",
-        pathname: "**",
         protocol: "https",
-        port: "443",
+        hostname: "digitalhippo-production-783c.up.railway.app",
       },
     ],
   },
