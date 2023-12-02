19207c393aeb0a2515e15e71b79602f332924243
yahay05 <94457180+yahay05@users.noreply.github.com>
2023-12-02 11:18:45 +0400

    add mobile nav component and import

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,6 +7,7 @@ import Cart from "./Cart"
 import { getServerSideUser } from "@/lib/payload-utils"
 import { cookies } from 'next/headers'
 import UserAccountNav from "./UserAccountNav"
+import MobileNav from "./MobileNav"
 const Navbar = async () => {
     const nextCookies = cookies()
     const { user }  = await getServerSideUser(nextCookies)
@@ -17,7 +18,7 @@ const Navbar = async () => {
                 <MaxWidthWrapper>
                     <div className="border-b border-gray-200">
                         <div className="flex h-16 items-center">
-                            {/* TODO: Mobile Nav */}
+                            <MobileNav />
 
                             <div className="ml-4 flex lg:ml-0">
                                 <Link href="/"><Icons.logo className="h-10 w-10"/></Link>
