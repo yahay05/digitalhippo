cbc29eb42faf65cb79f56faaddcc6b3f70af5d3d
yahay05 <94457180+yahay05@users.noreply.github.com>
2023-12-02 12:58:24 +0400

    addedd products page, footer and metadata

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
@@ -23,4 +24,48 @@ export function formatPrice(
       maximumFractionDigits: 2
     }).format(numericPrice)
   )
+}
+
+
+export function constructMetadata({
+  title = 'DigitalHippo - the marketplace for digital assets',
+  description = 'DigitalHippo is an open-source marketplace for high-quality digital goods.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@anonim',
+    },
+    icons,
+    metadataBase: new URL('https://digitalhippo-production-783c.up.railway.app/'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
 }
